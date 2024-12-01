fbcf7eab88e8796e18abb331bb9a7e94aedbc5f8
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2024-12-01 20:29:14 +0530

    created StartupCard component, and setup config for showing images from url

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchForm from '../../components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
 async function Home({
   searchParams,
@@ -7,6 +8,25 @@ async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+
+      author: {
+        _id: 1,
+        name: 'John',
+      },
+      _id: 1,
+      description: 'This is a description',
+      image:
+        'https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      category: 'Robots',
+      title: 'We Robots',
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -18,6 +38,18 @@ async function Home({
           competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0
+            ? posts.map((post) => <StartupCard key={post._id} post={post} />)
+            : null}
+        </ul>
       </section>
     </>
   );
